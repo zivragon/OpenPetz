07193f9fcb921d222b6a65aa50c165cbca29d1df
Ziverre <97392652+Ziverre@users.noreply.github.com>
2025-01-26 09:02:28 +0330

    Update Ball.cs

--- src/geometry/Ball.cs
+++ src/geometry/Ball.cs
@@ -80,23 +80,13 @@ public partial class Ball : MeshInstance2D
 	{
 		material.SetShaderParameter("center", this.GlobalPosition);
 
-		/*
 		 
 		// ROTATION TEST CODE
-		
-		// rotation.X += 0.0625f;
-		// rotation.Y += 0.0625f;
-		// rotation.Z = 0;
-		
-		// rotation.X += 0.0625f;
-		// rotation.Y = 0;
-		// rotation.Z = 0;
 		
 		rotation.X = 0;
 		rotation.Y += 0.0625f;
 		rotation.Z = 0;
 
-		*/
 	}
 
 	private void drawQuad(int size)
