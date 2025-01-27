701d4871e046c64d2fc4d285e0e8e2bd1fb635ff
nullpainter <nullpainter@seznam.cz>
2025-01-27 12:19:34 +0100

    fix - StringName leaking

---
+++ src/Manager/StringManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+public static class StringManager
+{
+    static Dictionary<string, StringName> _stringNames = new Dictionary<string, StringName>();
+
+    // Some Godot string cause the creation of StringName every frame
+    // this seems to leak memory, possibly, because Godot doesn't like to reclaim it, but 
+    // allocates happily every frame
+    public static StringName S(string str)
+    {
+        if (!_stringNames.ContainsKey(str))
+        {
+            _stringNames[str] = new StringName(str);
+        }
+
+        return _stringNames[str];
+    }
+}
