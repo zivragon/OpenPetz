61df1cdc812b50c59bfe5840065266f27635db9f
nullpainter <nullpainter@seznam.cz>
2025-02-26 19:37:36 +0100

    Make Ziv's code compile.

--- src/Utility/TextureAtlas.cs
+++ src/Utility/TextureAtlas.cs
@@ -3,9 +3,9 @@ using System;
 using System.Collections.Generic;
 using OpenPetz;
 
-public class TextureAtlas : Node2D { //TO DO: Replace with Node
+public partial class TextureAtlas : Node2D { //TO DO: Replace with Node
     
-    public static Vector2I MaximumSize => return new Vector2I(1024, 1024);
+    public static Vector2I MaximumSize => new Vector2I(1024, 1024);
     
     private SubViewport subViewport = null;
     private List<Texture2D> textureList = null;
@@ -17,13 +17,13 @@ public class TextureAtlas : Node2D { //TO DO: Replace with Node
     
     public Vector2I Size { get; private set; } = new Vector2I(1024, 64);
     
-    public TexturrAtlas (Guid _guid, List<Linez.Entries.Texture> _textureList)
+    public TextureAtlas(Guid _guid, List<OpenPetz.Linez.Entries.Texture> _textureList)
     {
         //First step is checking to see if it is already cached.
         
-        string fileName = "./cache/texture_atlas/raster/"+guid.ToString()+".bmp";
+        string fileName = "./cache/texture_atlas/raster/"+_guid.ToString()+".bmp";
         
-        if (FileAccess.FileExists(fileName)
+        if (FileAccess.FileExists(fileName))
         {
             // Load the cache instead
             /*Bmp bitmap = new Bmp();
@@ -40,7 +40,7 @@ public class TextureAtlas : Node2D { //TO DO: Replace with Node
         } else {
             //We are bound to dynamically generating it now using SubViewport.
             subViewport = new SubViewport();
-            subViewport.Size = Vector2I(1024, 64);
+            subViewport.Size = new Vector2I(1024, 64);
             subViewport.RenderTargetUpdateMode = SubViewport.UpdateMode.Once;
             AddChild(subViewport);
             
@@ -58,34 +58,34 @@ public class TextureAtlas : Node2D { //TO DO: Replace with Node
     
     public SubTextureCoordinations GetSubTextureCoords(int _index, int _color)
     {
-        ver subTex = subTexList[_index];
+        var subTex = subTexList[_index];
         
         if (subTex.Transparency == 0)
         {
-            return new SubTextureCoordinations(subTex.Dest.X / Size.X, subTexList.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
+            return new SubTextureCoordinations(subTex.Dest.X / Size.X, subTex.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
         }
         
         if (subTex.Transparency == 1)
         {
             int color = _color / 10;
             float moveByX = subTex.Size.X * color + subTex.Dest.X;
-            return new SubTextureCoordinations(moveByX / Size.X, subTexList.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
+            return new SubTextureCoordinations(moveByX / Size.X, subTex.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
         }
         
-        return new SubTextureCoordinations(subTex.Dest.X / Size.X, subTexList.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
+        return new SubTextureCoordinations(subTex.Dest.X / Size.X, subTex.Dest.Y / Size.Y, subTex.Size.X / Size.X, subTex.Size.Y / Size.Y);
     }
     
     // HEAVILY WIP
     
     private void PackTextures()
     {
-        texture = TextureManager.FetchTexture("./art/textures/flower.bmp");
-		palette = PaletteManager.FetchPalette("oddballz");
+        var texture = TextureManager.FetchTexture("./art/textures/flower.bmp");
+		var palette = PaletteManager.FetchPalette("oddballz");
 		
-		dummyMesh = new MeshInstance2D();
+		var dummyMesh = new MeshInstance2D();
 		
-		immediateMesh = new ImmediateMesh();
-		material = ShaderManager.FetchShaderMaterial("texture_atlas/texture");
+		var immediateMesh = new ImmediateMesh();
+		var material = ShaderManager.FetchShaderMaterial("texture_atlas/texture");
 		
 		dummyMesh.Mesh = immediateMesh;
 		dummyMesh.Material = material;
@@ -108,11 +108,11 @@ public class TextureAtlas : Node2D { //TO DO: Replace with Node
 		material.SetShaderParameter("palette", palette);
 		
 		var subTex = new SubTextureContainer();
-		subTex.Dest = new Vector2(0.0,0.0);
-		subTex.Size = new Vector2(64.0,64.0);
+		subTex.Dest = new Vector2(0.0f,0.0f);
+		subTex.Size = new Vector2(64.0f,64.0f);
 		subTex.Transparency = 0;
 		
-		subTexList.push(subTex);
+		subTexList.Add(subTex);
     }
     
     private void SaveGeneratedAtlas()
@@ -129,24 +129,28 @@ public class TextureAtlas : Node2D { //TO DO: Replace with Node
         RemoveChild(subViewport);
         subViewport = null;
         
-        Image img = Image.LoadFromFile("./cache/texture_atlas/raster/"+guid.ToString()+".png");
-        TextureData = ImageTexture.CreateFromImage(img);
+        Image img2 = Image.LoadFromFile("./cache/texture_atlas/raster/"+guid.ToString()+".png");
+        TextureData = ImageTexture.CreateFromImage(img2);
     }
 }
 
 //
 
 internal struct SubTextureContainer {
-    
-    public Vector2 Dest { get; set; } = new Vector2(0.0, 0.0);
-    public Vector2 Size { get; set; } = new Vector2(1.0, 1.0);
+	public SubTextureContainer()
+	{
+		;
+	}
+
+	public Vector2 Dest { get; set; } = new Vector2(0.0f, 0.0f);
+    public Vector2 Size { get; set; } = new Vector2(1.0f, 1.0f);
     public int Transparency { get; set; } = 0;
 }
 
 public struct SubTextureCoordinations {
     
-    public Vector2 Dest { get; private set; } = new Vector2(0.0, 0.0);
-    public Vector2 Size { get; private set; } = new Vector2(1.0, 1.0);
+    public Vector2 Dest { get; private set; } = new Vector2(0.0f, 0.0f);
+    public Vector2 Size { get; private set; } = new Vector2(1.0f, 1.0f);
     
     public SubTextureCoordinations (float _x, float _y, float _width, float _height)
     {
