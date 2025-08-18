49392a2f7d628254a470e560095079d0a6b88606
Ziv <97392652+zivragon@users.noreply.github.com>
2025-08-18 16:06:27 +0330

    Add files via upload

--- src/Graphics/Manager/MeshManager.cs
+++ src/Graphics/Manager/MeshManager.cs
@@ -1,3 +1,5 @@
+//TO DO: RE DO
+
 using Godot;
 using System;
 using System.Collections.Generic;
@@ -7,6 +9,8 @@ public static class MeshManager {
 	private static Dictionary<string, Mesh> avialableMeshes = new Dictionary<string, Mesh>();
 	
 	private static Mesh defaultMesh = null;
+	private static Mesh defaultLineMesh = null;
+	private static Mesh normalMesh = null;
 	
 	//Methods
 	
@@ -35,11 +39,75 @@ public static class MeshManager {
 		return defaultMesh;
 	}
 	
+	private static Mesh CreateNormalMesh()
+	{
+		var st = new SurfaceTool();
+
+		st.Begin(Mesh.PrimitiveType.Triangles);
+			
+		st.AddVertex(new Vector3(0, 0, 0));
+			
+		st.AddVertex(new Vector3(1, 0, 0));
+			
+		st.AddVertex(new Vector3(1, 1, 0));
+			
+		st.AddVertex(new Vector3(0, 0, 0));
+			
+		st.AddVertex(new Vector3(0, 1, 0));
+			
+		st.AddVertex(new Vector3(1, 1, 0));
+
+		// And finally, generate the mesh.
+		var meshBuffer = st.Commit();
+		normalMesh = meshBuffer;
+		
+		return normalMesh;
+	}
+	
+	private static Mesh CreateDefaultLineMesh()
+	{
+		var st = new SurfaceTool();
+
+		st.Begin(Mesh.PrimitiveType.Triangles);
+			
+		st.AddVertex(new Vector3(1, -1, 0));
+			
+		st.AddVertex(new Vector3(0, -1, 0));
+			
+		st.AddVertex(new Vector3(0, 1, 0));
+			
+		st.AddVertex(new Vector3(0, 1, 0));
+			
+		st.AddVertex(new Vector3(1, 1, 0));
+			
+		st.AddVertex(new Vector3(1, -1, 0));
+
+		// And finally, generate the mesh.
+		var meshBuffer = st.Commit();
+		defaultLineMesh = meshBuffer;
+		
+		return defaultLineMesh;
+	}
+	
 	public static Mesh FetchDefaultMesh()
 	{
 		if (defaultMesh == null)
 			defaultMesh = CreateDefaultMesh();
 		return defaultMesh; 
+	}
+	
+	public static Mesh FetchDefaultLineMesh()
+	{
+		if (defaultLineMesh == null)
+			defaultLineMesh = CreateDefaultLineMesh();
+		return defaultLineMesh; 
+	}
+	
+	public static Mesh FetchNormalMesh()
+	{
+		if (normalMesh == null)
+			normalMesh = CreateNormalMesh();
+		return normalMesh; 
 	}
 	
 	public static Mesh FetchMesh(string name)
