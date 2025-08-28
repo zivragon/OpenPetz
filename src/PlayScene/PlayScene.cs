fd5a34da70605b46a60e7c9056bb4448823c8cd3
Ziv <97392652+zivragon@users.noreply.github.com>
2025-08-28 19:41:39 +0330

    Add files via upload

---
+++ src/PlayScene/PlayScene.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using OpenPetz;
+
+//To Do: manage the background rendering a better way
+
+public partial class PlayScene : Node2D
+{
+	private Texture2D Background = null;
+	
+	public override void _Ready()
+	{
+		GD.Print("damn");
+		string bgPath = "./Resource/Area/Arabia/arabia.png";
+		
+		Image img = Image.LoadFromFile(bgPath);
+		Background = ImageTexture.CreateFromImage(img);
+		
+		var bg = new MeshInstance2D();
+		bg.Mesh = MeshManager.FetchNormalMesh();
+		
+		var mat = ShaderManager.FetchShaderMaterial("background");
+		mat.SetShaderParameter(StringManager.S("tex"), Background);
+		mat.SetShaderParameter(StringManager.S("size"), new Vector2(1024f, 768f));
+		
+		bg.Material = mat;
+		
+		bg.ZIndex = -999;
+		
+		AddChild(bg);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+
+}
